972d3b1e69b0b9809f3b0dfe2c49fd4289aa7ccb
Emilian Murawski <murawski.emilian@gmail.com>
2023-12-06 23:40:56 +0100

    update testScenario

--- Laboratorium-2/src/readme.md
+++ Laboratorium-2/src/readme.md
@@ -8,10 +8,10 @@ Scenariusze Testowe
 ### 1.4. Kliknij przycisk "Login"
 ### 1.5. Sprawdz czy zostałeś zalogowany(dokonaj asercji sciezki w przeglądarce)
 
-## 2. Testowanie funkcji dodawania produktu do koszyka na stronie [gandalf.com.pl](https://www.gandalf.com.pl)
-### 2.1. Wejdz na strone gandalf.com.pl
-### 2.2. Wyszukaj w polu wyszukiwania "Pismo Święte"
+## 2. Testowanie funkcji dodawania produktu do kolekcji na stronie [demoqa.com](https://demoqa.com/)
+### 2.1. Wejdz na strone demoqa.com/books
+### 2.2. Wyszukaj w polu wyszukiwania "Speaking JavaScript"
 ### 2.3. Kliknij przycisk "Szukaj"
-### 2.4. Kliknij przycisk "Dodaj do koszyka" przy pierwszym wyszukanym produkcie
-### 2.5. Kliknij przycisk "Przejdź do koszyka"
-### 2.6. Sprawdz czy został dodany produkt do koszyka(dokonaj asercji elementów na stronie)
+### 2.4. Kliknij przycisk ksiażki "Speaking JavaScript"
+### 2.5. Kliknij przycisk "Dodaj do kolekcji"
+### 2.6. Dokonaj assercji czy ksiazka została dodana do kolekcjiup
--- Laboratorium-2/src/test/java/com/example/laboratorium2/TestScenerio.java
+++ Laboratorium-2/src/test/java/com/example/laboratorium2/TestScenerio.java
@@ -9,15 +9,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.time.Duration;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class TestScenerio {
     private WebDriver driver;
+    private final Logger logger = Logger.getLogger(TestScenerio.class.getName());
 
     private static Stream<WebDriver> provideWebDrivers() {
         return Stream.of(
-                WebDriverManager.firefoxdriver().create(),
-                WebDriverManager.chromedriver().create());
+                WebDriverManager.firefoxdriver().create());
     }
 
     @ParameterizedTest
@@ -25,6 +27,8 @@ public class TestScenerio {
     void shouldSuccessfullyLoginAndReachInventorySite(WebDriver driver) {
 
         this.driver = driver;
+        logger.info("Test started");
+        logger.info("Opening browser");
         driver.get("https://www.saucedemo.com/");
         WebElement usernameTextbox = driver.findElement(By.id("user-name"));
         usernameTextbox.sendKeys("standard_user");
@@ -32,10 +36,40 @@ public class TestScenerio {
         passwordTextbox.sendKeys("secret_sauce");
         WebElement submitButton = driver.findElement(By.id("login-button"));
         submitButton.click();
-        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());    
-        
-        driver.quit();
+        Assertions.assertEquals("https://www.saucedemo.com/inventory.html", driver.getCurrentUrl());
+        logger.info("Test finished");
     }
+
+    @ParameterizedTest
+    @MethodSource("provideWebDrivers")
+    void shouldOpenBookStoreSiteAndSearchForBookAndAddItToCart(WebDriver driver) {
+
+        this.driver = driver;
+        driver.get("https://demoqa.com/books");
+        driver.manage().window().maximize();
+        WebElement loginButton = driver.findElement(By.id("login"));
+        loginButton.click();
+        WebElement usernameTextbox = driver.findElement(By.id("userName"));
+        usernameTextbox.sendKeys("testuser_1");
+        WebElement passwordTextbox = driver.findElement(By.id("password"));
+        passwordTextbox.sendKeys("Test@123");
+        WebElement submitButton = driver.findElement(By.id("login"));
+        submitButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        WebElement searchBox = driver.findElement(By.id("searchBox"));
+        searchBox.sendKeys("Speaking JavaScript");
+        WebElement searchButton = driver.findElement(By.id("basic-addon2"));
+        searchButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        WebElement book = driver.findElement(By.xpath("//*[@id=\"see-book-Speaking JavaScript\"]"));
+        book.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        WebElement addToYourCollectionButton = driver.findElement(By.id("addNewRecordButton"));
+        addToYourCollectionButton.click();
+        Assertions.assertEquals("Book added to your collection.", driver.findElement(By.id("closeSmallModal-ok")).getText());
+    }
+
+
 
     @AfterEach
     void tearDown() {
@@ -43,5 +77,4 @@ public class TestScenerio {
             driver.quit();
         }
     }
-
 }
